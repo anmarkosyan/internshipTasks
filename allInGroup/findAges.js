d5ae8d63c51ff52a6da4d916962ba65aa2dc7dd0
anmarkosyan <anush.markos@gmail.com>
2020-09-27 11:01:59 +0400

    added Math.round for any cases

--- allInGroup/findAges.js
+++ allInGroup/findAges.js
@@ -3,7 +3,7 @@
 // I will be 79 years old." How old both of them are now? *
 
 const findCurrAges = (num1, num2) => {
-    let x = (num2 + 8) / 3;
+    let x = Math.round((num2 + 8) / 3);
     let sonAge = x;
     let fatherAge = num2 - (x - num1)
     return [sonAge, fatherAge]

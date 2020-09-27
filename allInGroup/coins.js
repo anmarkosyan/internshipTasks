36cc880dbe483bfade2adc8199f82d1d59973dc1
anmarkosyan <anush.markos@gmail.com>
2020-09-27 11:55:18 +0400

    8 coins problem

---
+++ allInGroup/coins.js
@@ -0,0 +1,10 @@
+//============================ coins =================================
+//Out of 8 coins, one weighs less than the others. You have a scale.
+//How can you determine which one weighs less in 2 weigh? *
+
+
+//step 1 .at first we separate 8 coins within 3 part(3, 3, 2);
+//then we weight 3 and 3,
+//step 2. if there is equal we weight other two coins , so we  find out lighter.
+//if first three coins not equal each other, we again weight lighter part with cutting 2 part, and if not equal,
+//take lighter part and do it one more time, finally we find lighter coin.
